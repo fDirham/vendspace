254ec41ef09754514bc6dd8c51c62ce4bfb573a6
fDirham <fajarletters@gmail.com>
2022-12-12 15:55:24 -0800

    edit button to store blocks

--- components/home/StoreBlock/StoreBlock.tsx
+++ components/home/StoreBlock/StoreBlock.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { StoreInfo } from 'utilities/types';
 import styles from './StoreBlock.module.scss';
 
-type StoreBlockProps = { storeInfo: StoreInfo; onClick: () => void };
+type StoreBlockProps = {
+  storeInfo: StoreInfo;
+  onNavigate: () => void;
+  onEdit: () => void;
+};
 
-export default function StoreBlock({ storeInfo, onClick }: StoreBlockProps) {
+export default function StoreBlock({
+  storeInfo,
+  onNavigate,
+  onEdit,
+}: StoreBlockProps) {
   return (
-    <div className={styles.container} onClick={onClick}>
-      <p>{storeInfo.name}</p>
+    <div className={styles.container}>
+      <div className={styles.leftContainer} onClick={onNavigate}>
+        <p>{storeInfo.name}</p>
+      </div>
+      <div className={styles.rightContainer} onClick={onEdit}>
+        <img src='/icons/edit.png' alt='edit' className={styles.editIcon} />
+      </div>
     </div>
   );
 }
