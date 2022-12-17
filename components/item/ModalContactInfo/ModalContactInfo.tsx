e70488629e47b72bedf54bae9ccc68f1dc654eaf
fDirham <fajarletters@gmail.com>
2022-12-17 13:17:46 -0800

    Simple item page

---
+++ components/item/ModalContactInfo/ModalContactInfo.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './ModalContactInfo.module.scss';
+import ModalBase from 'components/all/ModalBase';
+import { ModalBaseProps } from 'components/all/ModalBase/ModalBase';
+import { StoreInfo } from 'utilities/types';
+
+type ModalContactInfoProps = {
+  open: boolean;
+  onClose: () => void;
+  storeInfo: StoreInfo;
+};
+
+export default function ModalContactInfo(props: ModalContactInfoProps) {
+  const renderContent = (modalProps: ModalBaseProps) => {
+    return (
+      <>
+        <p className={styles.sectionTitle}>Contact info</p>
+        <p className={styles.sectionText}>{props.storeInfo.contact}</p>
+        <p className={styles.sectionTitle}>Payment info</p>
+        <p className={styles.sectionText}>{props.storeInfo.payment}</p>
+      </>
+    );
+  };
+
+  return (
+    <ModalBase
+      open={props.open}
+      onClose={props.onClose}
+      renderContent={renderContent}
+      containerClassName={styles.container}
+    />
+  );
+}
--- pages/s/[handle]/[storeId]/[itemId].tsx
+++ pages/s/[handle]/[storeId]/[itemId].tsx
@@ -1,19 +1,34 @@
 import ControllerItems from 'controllers/ControllerItems';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import { ItemInfo } from 'utilities/types';
+import { ItemInfo, PublicUserData, StoreInfo } from 'utilities/types';
 import styles from './ItemPage.module.scss';
 import VisualSystem from 'components/item/VisualSystem';
 import PageContainer from 'components/all/PageContainer';
+import StyledButton from 'components/all/StyledButton';
+import ControllerAuth from 'controllers/ControllerAuth';
+import ControllerStores from 'controllers/ControllerStores';
+import ModalContactInfo from 'components/item/ModalContactInfo';
 
 export default function itemPage() {
   const router = useRouter();
   const { handle, storeId, itemId } = router.query;
   const [itemInfo, setItemInfo] = useState<ItemInfo>();
+  const [storeInfo, setStoreInfo] = useState<StoreInfo>();
+  const [sellerData, setSellerData] = useState<PublicUserData>();
+  const [openInfo, setOpenInfo] = useState<boolean>(false);
 
   useEffect(() => {
     getItemInfo();
   }, [handle, storeId, itemId]);
+
+  useEffect(() => {
+    getStoreInfo();
+  }, [handle, storeId]);
+
+  useEffect(() => {
+    getSellerInfo();
+  }, [handle]);
 
   async function getItemInfo() {
     if (!handle || !storeId || !itemId) return;
@@ -27,10 +42,49 @@ export default function itemPage() {
     }
   }
 
-  if (!itemInfo) return;
+  async function getStoreInfo() {
+    if (!handle || !storeId) return;
+    const getRes = await ControllerStores.getStoreInfo(
+      handle as string,
+      storeId as string
+    );
+    if (!getRes.isError) {
+      setStoreInfo(getRes.store!);
+    }
+  }
+
+  async function getSellerInfo() {
+    if (!handle) return;
+    const getRes = await ControllerAuth.getPublicUserData(handle as string);
+    if (!getRes.isError) {
+      setSellerData(getRes.userData!);
+    }
+  }
+
+  if (!itemInfo || !storeInfo || !sellerData) return;
   return (
     <PageContainer className={styles.container}>
+      <ModalContactInfo
+        open={openInfo}
+        onClose={() => setOpenInfo(false)}
+        storeInfo={storeInfo}
+      />
       <VisualSystem visuals={itemInfo?.visuals} />
+      <div className={styles.infoContainer}>
+        <h1>{itemInfo.name}</h1>
+        <p className={styles.whoText}>
+          <span className={styles.storeText}>{storeInfo.name}</span>
+          {' - '}
+          <span className={styles.sellerName}>{sellerData.displayName}</span>
+        </p>
+        <p className={styles.price}>{itemInfo.price}</p>
+        <p className={styles.description}>{itemInfo.description}</p>
+      </div>
+      <div className={styles.actionContainer}>
+        <StyledButton onClick={() => setOpenInfo(true)}>
+          contact seller
+        </StyledButton>
+      </div>
     </PageContainer>
   );
 }
