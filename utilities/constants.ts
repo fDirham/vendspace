345d5d6e78bae06610c9082181c49862be0fe5ce
fDirham <fajarletters@gmail.com>
2022-12-22 21:46:08 -0800

    changed length of names

--- utilities/constants.ts
+++ utilities/constants.ts
@@ -9,12 +9,12 @@ export const MIN_LENGTH_HANDLE = 4;
 export const REGEX_HANDLE = /^[a-z0-9]{4,12}$/i;
 
 // Store fields
-export const MAX_LENGTH_STORE_NAME = 40;
+export const MAX_LENGTH_STORE_NAME = 100;
 export const MAX_LENGTH_STORE_DETAIL = 40;
 export const MAX_LENGTH_STORE_DESCRIPTION = 200;
 
 // Item fields
-export const MAX_LENGTH_ITEM_NAME = 40;
+export const MAX_LENGTH_ITEM_NAME = 100;
 export const MAX_LENGTH_ITEM_DESCRIPTION = 700;
 export const MAX_LENGTH_ITEM_PRICE = 20;
 export const MAX_ITEM_VISUALS = 5;
