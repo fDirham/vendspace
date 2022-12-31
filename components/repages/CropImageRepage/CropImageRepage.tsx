174562d1f1e0fbfa396cf8a929398fcc4c96145d
fDirham <fajarletters@gmail.com>
2022-12-30 23:49:04 -0800

    cross origin rollback

--- components/repages/CropImageRepage/CropImageRepage.tsx
+++ components/repages/CropImageRepage/CropImageRepage.tsx
@@ -40,8 +40,6 @@ export default function CropImageRepage(props: CropImageRepageProps) {
             setCropper(instance);
           }}
           guides={true}
-          crossOrigin=''
-          checkCrossOrigin
         />
       </div>
       <StyledButton
