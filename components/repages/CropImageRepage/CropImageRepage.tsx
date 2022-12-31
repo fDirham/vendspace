0a53ae221799eb3e42195978052007192a2f49fd
fDirham <fajarletters@gmail.com>
2022-12-30 23:36:42 -0800

    cross origin blank

--- components/repages/CropImageRepage/CropImageRepage.tsx
+++ components/repages/CropImageRepage/CropImageRepage.tsx
@@ -40,7 +40,7 @@ export default function CropImageRepage(props: CropImageRepageProps) {
             setCropper(instance);
           }}
           guides={true}
-          crossOrigin='anonymous'
+          crossOrigin=''
           checkCrossOrigin
         />
       </div>
